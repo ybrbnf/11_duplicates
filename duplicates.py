ebdccfd483d42777ca5c38abc8598e2e84c22bc2
Nikita Tkachenko <ybrbnf418@gmail.com>
2016-10-06 13:18:46 +0300

    ver1.0

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,24 @@
+import os
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+def are_files_duplicates(file_path):
+    lst = []
+    result = []
+    for root, dirs, files in os.walk(file_path):
+        for item in files:
+            file_name = os.path.join(root, item)
+            file_size = os.path.getsize(file_name)
+            temp = (item, file_size)
+            if temp not in lst:
+                lst.append(temp)
+            else:
+                result.append(file_name)
+    return result
 
 
 if __name__ == '__main__':
-    pass
+    file_path = input('Введите путь к папке: ')
+    result = are_files_duplicates(file_path)
+    for item in result:
+        os.remove(item)
+        print ('Удален файл:', item)
